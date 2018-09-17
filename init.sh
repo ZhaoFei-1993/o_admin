27961aa88cf8bd8400038b9fadacf67beb7428ce
dengshen <dengshendengshen@gmail.com>
2018-09-17 10:58:21 +0800

    feat: add log rotate

--- init.sh
+++ init.sh
@@ -1,1 +1,1 @@
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - && sudo apt install -y nodejs && sudo npm i -g pm2 && pm2 install pm2-intercom && npm i
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - && sudo apt install -y nodejs && sudo npm i -g pm2 && pm2 install pm2-intercom && pm2 install pm2-logrotate && pm2 set pm2-logrotate:max_size 100M && npm i
